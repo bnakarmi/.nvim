08fb5ff98ef7fa6c1b2cd2a37ef48e99dde83bb3
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2022-12-17 13:52:31 -0500

    Formatting

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -1,37 +1,37 @@
 require('nvim-lsp-installer').setup({
-    -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-    automatic_installation = true,
-    ui = {
-        icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
-        }
+  -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+  automatic_installation = true,
+  ui = {
+    icons = {
+      server_installed = "✓",
+      server_pending = "➜",
+      server_uninstalled = "✗"
     }
+  }
 })
 
 local lsp_defaults = {
-    flags = {
-        debounce_text_changes = 150,
-    },
-    capabilities = require('cmp_nvim_lsp').default_capabilities(
-        vim.lsp.protocol.make_client_capabilities()
-    ),
-    on_attach = function(client, bufnr)
-        if client.name == 'tsserver' then
-            client.server_capabilities.document_formatting = false
-        end
-
-        vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+  flags = {
+    debounce_text_changes = 150,
+  },
+  capabilities = require('cmp_nvim_lsp').default_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+  ),
+  on_attach = function(client, bufnr)
+    if client.name == 'tsserver' then
+      client.server_capabilities.document_formatting = false
     end
+
+    vim.api.nvim_exec_autocmds('User', { pattern = 'LspAttached' })
+  end
 }
 
 local lspconfig = require('lspconfig')
 
 lspconfig.util.default_config = vim.tbl_deep_extend(
-    'force',
-    lspconfig.util.default_config,
-    lsp_defaults
+  'force',
+  lspconfig.util.default_config,
+  lsp_defaults
 )
 
 -- Setup LSP
@@ -46,67 +46,67 @@ lspconfig['dartls'].setup({})
 
 -- Setup keybinding
 vim.api.nvim_create_autocmd('User', {
-    pattern = 'LspAttached',
-    desc = 'LSP actions',
-    callback = function()
-        local bufmap = function(mode, lhs, rhs)
-            local opts = { buffer = true }
+  pattern = 'LspAttached',
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = { buffer = true }
 
-            vim.keymap.set(mode, lhs, rhs, opts)
-        end
-
-        local organize_imports = function(bufnr)
-            if not bufnr then bufnr = vim.api.nvim_get_current_buf() end
-
-            local params = {
-                command = "_typescript.organizeImports",
-                arguments = { vim.api.nvim_buf_get_name(bufnr) },
-                title = "Organize imports"
-            }
-
-            vim.lsp.buf.execute_command(params)
-        end
-
-        -- Displays hover information about the symbol under the cursor
-        bufmap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
-        -- Jump to the definition
-        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-
-        -- Jump to declaration
-        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-
-        -- Lists all the implementations for the symbol under the cursor
-        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-
-        -- Jumps to the definition of the type symbol
-        bufmap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-
-        -- Lists all the references
-        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-
-        -- Displays a function's signature information
-        bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
-        -- Organize imports
-        bufmap('n', 'go', organize_imports)
-
-        -- Renames all references to the symbol under the cursor
-        bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
-        -- Selects a code action available at the current cursor position
-        bufmap('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-        bufmap('x', '<leader>.', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-
-        -- Show diagnostics in a floating window
-        bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
-
-        -- Move to the previous diagnostic
-        bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-
-        -- Move to the next diagnostic
-        bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-
-        vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format{ async = true }' ]]
+      vim.keymap.set(mode, lhs, rhs, opts)
     end
+
+    local organize_imports = function(bufnr)
+      if not bufnr then bufnr = vim.api.nvim_get_current_buf() end
+
+      local params = {
+        command = "_typescript.organizeImports",
+        arguments = { vim.api.nvim_buf_get_name(bufnr) },
+        title = "Organize imports"
+      }
+
+      vim.lsp.buf.execute_command(params)
+    end
+
+    -- Displays hover information about the symbol under the cursor
+    bufmap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+    -- Lists all the references
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Organize imports
+    bufmap('n', 'go', organize_imports)
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<leader>.', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<leader>.', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Show diagnostics in a floating window
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+    -- Move to the previous diagnostic
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+    -- Move to the next diagnostic
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+    vim.cmd [[ command! Format execute 'lua vim.lsp.buf.format{ async = true }' ]]
+  end
 })
