2c98af728b64eec72cf2439a4bc57e788441c8b4
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2022-12-18 14:44:48 -0500

    Updated configs

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,1 +1,11 @@
-require('lualine').setup({})
+require('lualine').setup({
+    options = {
+        icons_enabled = true,
+        theme = 'ayu_dark',
+        refresh = {
+            statusline = 1000,
+            tabline = 1000,
+            winbar = 1000,
+        }
+    }
+})
