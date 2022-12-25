8ed415781ec2aa52c6943e03405f03889e2a06a4
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2022-12-25 09:48:47 -0500

    Reset colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,3 +2,5 @@ vim.g.ayucolor = "darker"
 
 vim.cmd.syntax("enable")
 vim.cmd.colorscheme("ayu")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
