3f7eb5abb2ec697c273d5a27e97c3118e33c7cc9
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2023-02-03 22:21:53 -0500

    Updated colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,6 +1,4 @@
-vim.g.ayucolor = "darker"
-
 vim.cmd.syntax("enable")
-vim.cmd.colorscheme("nightfox")
+vim.cmd.colorscheme("carbonfox")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
