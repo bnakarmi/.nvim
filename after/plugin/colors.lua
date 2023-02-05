aa5fb63881ff081830e001755147c52b93e21869
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2023-02-05 15:42:35 -0500

    Updated colorscheme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,2 +1,6 @@
 vim.cmd.syntax("enable")
 vim.cmd.colorscheme("carbonfox")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
