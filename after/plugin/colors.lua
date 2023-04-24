d10b14b0a94a4995f46176cfe3931e48dc5c6368
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2023-04-24 08:49:16 -0400

    Colorscheme changes

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,2 @@
 vim.cmd.syntax("enable")
-
-function Color(scheme)
-    scheme = scheme or "tokyonight-night"
-
-    vim.cmd.colorscheme(scheme)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
-end
-
-Color()
+vim.cmd.colorscheme("tokyonight-night")
