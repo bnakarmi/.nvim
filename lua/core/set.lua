581468c9c960dca8cf84538fa9e0aa32e5a22670
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2023-05-31 15:57:12 -0400

    Cleanup

--- lua/core/set.lua
+++ lua/core/set.lua
@@ -1,40 +1,23 @@
 vim.opt.exrc = true
 vim.opt.mouse = "a"
-vim.opt.hidden = true
-
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
-
 vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.errorbells = false
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.autoindent = true
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
-vim.opt.showmode = true
-
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = "80"
 vim.opt.signcolumn = "yes:1"
-vim.opt.background = "dark"
-
 vim.opt.updatetime = 50
-
 vim.opt.splitright = true
 vim.opt.splitbelow = true
-
 vim.opt.cursorline = true
 vim.opt.statusline = "%f %M %Y %R %= %l:%c %p%%"
 vim.opt.laststatus = 3
-
 vim.g.netrw_liststyle = 3
-
 vim.g.mapleader = " "
