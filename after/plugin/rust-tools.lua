cdeb412ef39f7b191ba9be7bcb3b9e85f17d57d9
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2023-07-15 13:56:48 -0400

    Cleanup

--- after/plugin/rust-tools.lua
+++ after/plugin/rust-tools.lua
@@ -1,16 +1,6 @@
-local dap = require("dap")
 local rt = require("rust-tools")
-local mason_registry = require("mason-registry")
-
-local codelldb = mason_registry.get_package("codelldb")
-local ext_path = codelldb:get_install_path() .. "/extension/"
-local codelldb_path = ext_path .. "adapter/codelldb"
-local liblldb_path = ext_path .. "lldb/lib/liblldb.dylib"
 
 rt.setup({
-    dap = {
-        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-    },
     server = {
         capabilities = require("cmp_nvim_lsp").default_capabilities(),
         on_attach = function(_, bufnr)
@@ -24,21 +14,3 @@ rt.setup({
     }
 })
 
-dap.adapters.codelldb = {
-    type = "server",
-    host = "127.0.0.1",
-    port = 13000
-}
-
-dap.configurations.rust = {
-    {
-        type = "codelldb",
-        request = "launch",
-        program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        terminal = "integrated",
-        sourceLanguages = { "rust" }
-    }
-}
--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
 require("core")
-require("dap_core")
