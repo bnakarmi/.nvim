91e1a53c5f82974e394fc228f5c58d7e2edce198
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2024-06-27 21:00:09 -0400

    go snip update

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("i", "<leader>ife", "if err != nil {<cr><cr>}<up><tab>", { desc = "[GO] - if err snippet" })
+vim.keymap.set("i", "<leader>ife", "if err != nil {<cr><cr>}<up><tab><tab>", { desc = "[GO] - if err snippet" })
