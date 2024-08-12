5293f6859739f3a65da619ce82b5dc8d82c7c126
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2024-08-12 19:39:53 -0400

    colorscheme change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,18 @@ return {
     {
         "nanotech/jellybeans.vim",
         priority = 1000,
+        enabled = false,
         config = function()
             vim.cmd.syntax("enable")
             vim.cmd.colorscheme("jellybeans")
+        end
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        config = function()
+            vim.cmd.syntax("enable")
+            vim.cmd.colorscheme("kanagawa-dragon")
         end
     }
 }
