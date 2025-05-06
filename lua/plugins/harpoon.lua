640a6c7ded424cc937abab6aa901cc70e2aca2c8
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2025-05-06 19:59:46 -0400

    removed rust tools

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
         harpoon:setup()
 
         vim.keymap.set("n", "<leader>ha", function()
-            harpoon:list():append()
+            harpoon:list():add()
         end, { desc = "[H]arpoon [M]ark" })
         vim.keymap.set("n", "<leader>hq", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
