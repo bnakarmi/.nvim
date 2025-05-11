ffe3a3391e7e42ab39fe26fb1362c27d078c2fdf
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2025-05-10 20:14:38 -0400

    no lsp to configure

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,22 +17,22 @@ return {
 
             require("mason-lspconfig").setup()
 
-            require("mason-lspconfig").setup_handlers({
-                -- Will be called for each installed server that doesn't have
-                -- a dedicated handler.
+            -- require("mason-lspconfig").setup_handlers({
+            --     -- Will be called for each installed server that doesn't have
+            --     -- a dedicated handler.
 
-                function(server_name) -- default handler (optional)
-                    -- https://github.com/neovim/nvim-lspconfig/pull/3232
-                    if server_name == "tsserver" then
-                        server_name = "ts_ls"
-                    end
+            --     function(server_name) -- default handler (optional)
+            --         -- https://github.com/neovim/nvim-lspconfig/pull/3232
+            --         if server_name == "tsserver" then
+            --             server_name = "ts_ls"
+            --         end
 
-                    local capabilities = require("cmp_nvim_lsp").default_capabilities()
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-            })
+            --         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            --         require("lspconfig")[server_name].setup({
+            --             capabilities = capabilities,
+            --         })
+            --     end,
+            -- })
 
             vim.keymap.set(
                 "n",
