218aa861c3232bdc5cf7c74b8fb0c5bf743c5258
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2026-04-16 11:08:58 -0400

    remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -12,7 +12,26 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 
 -- Close all hidden buffers
 vim.api.nvim_create_user_command("BufOnly", "%bdelete|edit #|bdelete #", {})
-vim.keymap.set("n", "<leader>cb", "<cmd>BufOnly<CR>")
+vim.keymap.set("n", "<leader>cab", "<cmd>BufOnly<CR>", { desc = "[C]lose all hidden buffers" })
+vim.keymap.set("n", "<leader>cb", function()
+    local current = vim.api.nvim_get_current_buf()
+    local alternate = vim.fn.bufnr("#")
+
+    if vim.bo[current].modified then
+        vim.notify("Buffer has unsaved changes", vim.log.levels.WARN)
+        return
+    end
+
+    if alternate ~= -1 and vim.api.nvim_buf_is_valid(alternate) then
+        vim.cmd("buffer #")
+    else
+        vim.cmd("enew")
+    end
+
+    if vim.api.nvim_buf_is_valid(current) then
+        vim.cmd("bdelete " .. current)
+    end
+end, { desc = "[C]lose buffer in current split and go to previous buffer" })
 
 -- Git
 vim.keymap.set("n", "<leader>gs", "<cmd>LazyGit<CR>",
@@ -23,7 +42,12 @@ vim.keymap.set("n", "<leader>ex", "<cmd>Ex<CR>",
     { desc = "[E]xplorer" })
 
 vim.keymap.set("v", "<leader>y", "\"+y", { desc = "[C]opy to Clipboard" })
+vim.keymap.set("n", "p", function()
+    local c = vim.fn.col(".")
+
+    vim.cmd("normal! p")
+    vim.fn.cursor(vim.fn.line("."), c)
+end, { desc = "[P]aste and restore column" })
 vim.keymap.set("n", "<leader>p", "\"+P", { desc = "[P]aste from Clipoard" })
 
 vim.keymap.set("t", "<leader><esc>", "<c-\\><c-n>", { desc = "[T]erminal::Switch to normal mode" })
-
