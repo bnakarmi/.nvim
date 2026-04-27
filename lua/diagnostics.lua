72293af2c1ef6ca2a00a72be81b42759aa3fb72a
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2026-04-27 09:36:41 -0400

    diagnostic cleanup

--- lua/diagnostics.lua
+++ lua/diagnostics.lua
@@ -1,16 +1,3 @@
-local sign = function(opts)
-    vim.fn.sign_define(opts.name, {
-        texthl = opts.name,
-        text = opts.text,
-        numhl = "",
-    })
-end
-
-sign({ name = "DiagnosticSignError", text = "✘" })
-sign({ name = "DiagnosticSignWarn", text = "▲" })
-sign({ name = "DiagnosticSignHint", text = "⚑" })
-sign({ name = "DiagnosticSignInfo", text = "" })
-
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "[P]revious [D]iagnostic" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "[N]ext [D]iagnostic" })
 vim.keymap.set("n", "do", vim.diagnostic.open_float, { desc = "[O]pen [D]iagnostic [E]rror" })
