4103d56cb98d29fc72927b39a875c5488df2b465
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2026-05-02 13:12:15 -0400

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,29 @@
 vim.cmd.syntax("enable")
 
 return {
-    -- {
-    --     "nanotech/jellybeans.vim",
-    --     priority = 1000,
-    --     config = function()
-    --         vim.cmd.colorscheme("jellybeans")
-    --
-    --         vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#c06c75" })
-    --         vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
-    --             fg = "#c06c75",
-    --             bg = "#2a1f22",
-    --         })
-    --         vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
-    --             sp = "#c06c75",
-    --         })
-    --         vim.api.nvim_set_hl(0, "DiagnosticLineNrError", {
-    --             fg = "#c06c75",
-    --             bg = "NONE",
-    --             bold = true,
-    --         })
-    --         vim.api.nvim_set_hl(0, "ErrorMsg", {
-    --             fg = "#c06c75",
-    --             bg = "#2a1f22",
-    --         })
-    --     end
-    -- },
     {
-        "tjdevries/colorbuddy.nvim",
-        lazy = false,
+        "nanotech/jellybeans.vim",
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme "gruvbuddy"
-        end,
-    }
+            vim.cmd.colorscheme("jellybeans")
 
+            vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#c06c75" })
+            vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
+                fg = "#c06c75",
+                bg = "#2a1f22",
+            })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
+                sp = "#c06c75",
+            })
+            vim.api.nvim_set_hl(0, "DiagnosticLineNrError", {
+                fg = "#c06c75",
+                bg = "NONE",
+                bold = true,
+            })
+            vim.api.nvim_set_hl(0, "ErrorMsg", {
+                fg = "#c06c75",
+                bg = "#2a1f22",
+            })
+        end
+    },
 }
