004c3b1e27d9d52a76e64b09b5bb0275cc48607a
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2026-05-02 10:38:01 -0400

    diagnostic color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,24 @@ return {
         priority = 1000,
         config = function()
             vim.cmd.colorscheme("jellybeans")
+
+            vim.api.nvim_set_hl(0, "DiagnosticError", { fg = "#c06c75" })
+            vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", {
+                fg = "#c06c75",
+                bg = "#2a1f22",
+            })
+            vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", {
+                sp = "#c06c75",
+            })
+            vim.api.nvim_set_hl(0, "DiagnosticLineNrError", {
+                fg = "#c06c75",
+                bg = "NONE",
+                bold = true,
+            })
+            vim.api.nvim_set_hl(0, "ErrorMsg", {
+                fg = "#c06c75",
+                bg = "#2a1f22",
+            })
         end
     }
 }
