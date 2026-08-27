b7125d4debf432b099838165f7df69f6816f9fd6
Bimarsh Nakarmi <nakarmibimarsh@gmail.com>
2026-08-27 11:58:58 -0400

    updated tresitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,28 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
+        branch = "main",
         build = ":TSUpdate",
         config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    'lua',
-                    'typescript',
-                    'javascript',
-                    'css',
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    disable = { 'html', 'yaml' },
-                    additional_vim_regex_highlighting = false
-                },
-                indent = { enable = true },
+            require("nvim-treesitter").install({
+                'lua',
+                'typescript',
+                'javascript',
+                'css',
+            })
+
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("TreesitterStart", { clear = true }),
+                callback = function(args)
+                    local disabled_fts = { html = true, yaml = true }
+                    local ft = vim.bo[args.buf].filetype
+
+                    if not disabled_fts[ft] then
+                        pcall(vim.treesitter.start, args.buf)
+                    end
+
+                    vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                end
             })
         end
     },
